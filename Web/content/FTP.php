e768d981b5a01f3838fbc8a6d0b08559f25b6afd
Michal Sedlák <michalsedlak35@gmail.com>
2024-11-13 19:06:31 +0100

    Added design of FTP

--- Web/content/FTP.php
+++ Web/content/FTP.php
@@ -6,3 +6,44 @@
         session_destroy();
     }
 ?>
+
+
+    <head>
+        <title>FTP</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="../css/bootstrap.css">
+        <script src="../js/bootstrap.bundle.js"></script>
+        <style>
+            .topRow{
+                height: 20vh;
+            }
+        </style>
+    </head>
+    <body class="container-fluid text-center">
+        <header class="row topRow p-5">
+            <h1 class="border-dark border-bottom">USB Raid pole</h1>
+        </header>
+
+        <section class="row">
+            <aside class="col">
+            </aside>
+
+            <article class="col-8 border border-2 border-primary rounded p-2">
+                <div class="col">
+
+                </div>
+
+                <div class="col">
+                    
+                </div>
+
+                <div class="col">
+                    
+                </div>
+            </article>
+
+            <aside class="col">
+            </aside>
+        </section>
+    </body>
