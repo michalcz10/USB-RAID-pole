c8dc5ffb18bf08a788b51097e3b3043a02e757ee
Michal Sedlák <michalsedlak35@gmail.com>
2025-03-19 19:21:24 +0100

    Added new current DB model

--- Documentation/usbraidlogin.sql
+++ Documentation/usbraidlogin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: localhost:3306
--- Vytvořeno: Stř 19. bře 2025, 17:31
+-- Vytvořeno: Stř 19. bře 2025, 18:18
 -- Verze serveru: 8.0.41-0ubuntu0.24.04.1
 -- Verze PHP: 8.3.6
 
@@ -31,15 +31,17 @@ CREATE TABLE `users` (
   `uname` varchar(255) NOT NULL,
   `pswd` varchar(60) NOT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
-  `defPath` varchar(255) NOT NULL DEFAULT '/'
+  `defPath` varchar(255) NOT NULL DEFAULT '/',
+  `delPer` tinyint(1) NOT NULL DEFAULT '0',
+  `downPer` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Vypisuji data pro tabulku `users`
 --
 
-INSERT INTO `users` (`uname`, `pswd`, `admin`, `defPath`) VALUES
-('admin', '$2y$10$gKSD.GVBedJ1DuND73YHheWmQuTSy.pFi5zLkyc6.5fzZPK8/ZG6W', 1, '/');
+INSERT INTO `users` (`uname`, `pswd`, `admin`, `defPath`, `delPer`, `downPer`) VALUES
+('admin', '$2y$10$FHV.EggKB58SVLGqIgWaXeEV6f4SFBjjEcDCr1J.mn2mSUdhGVva6', 1, '/', 1, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
