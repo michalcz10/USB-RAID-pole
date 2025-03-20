69ee0305c771abbc197944aa7ef9fae2ad7eda9e
Michal Sedlák <michalsedlak35@gmail.com>
2025-03-20 12:56:54 +0100

    Nová verze DB exportu

--- Documentation/usbraidlogin.sql
+++ Documentation/usbraidlogin.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1deb3
+-- version 5.1.1deb5ubuntu1
 -- https://www.phpmyadmin.net/
 --
 -- Počítač: localhost:3306
--- Vytvořeno: Stř 19. bře 2025, 18:18
--- Verze serveru: 8.0.41-0ubuntu0.24.04.1
--- Verze PHP: 8.3.6
+-- Vytvořeno: Čtv 20. bře 2025, 12:55
+-- Verze serveru: 8.0.41-0ubuntu0.22.04.1
+-- Verze PHP: 8.1.2-1ubuntu2.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,15 +33,16 @@ CREATE TABLE `users` (
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   `defPath` varchar(255) NOT NULL DEFAULT '/',
   `delPer` tinyint(1) NOT NULL DEFAULT '0',
-  `downPer` tinyint(1) NOT NULL DEFAULT '0'
+  `downPer` tinyint(1) NOT NULL DEFAULT '0',
+  `upPer` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Vypisuji data pro tabulku `users`
 --
 
-INSERT INTO `users` (`uname`, `pswd`, `admin`, `defPath`, `delPer`, `downPer`) VALUES
-('admin', '$2y$10$FHV.EggKB58SVLGqIgWaXeEV6f4SFBjjEcDCr1J.mn2mSUdhGVva6', 1, '/', 1, 1);
+INSERT INTO `users` (`uname`, `pswd`, `admin`, `defPath`, `delPer`, `downPer`, `upPer`) VALUES
+('admin', '$2y$10$QZEnBMv01f92Ikrta3Qo4ewbmoozVuDT.uPhtztNT7bsIwDQ.Fy8e', 1, '/', 1, 1, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
